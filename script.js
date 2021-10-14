6d8316294e2ca95c524efb4c9c9fc8cb85e4b45b
Davi Santos Fonseca <davi.santos.fonseca@gmail.com>
2021-10-13 21:07:45 -0300

    :package: Menu sidebar

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".menuh").addEventListener("click", () =>
+    document.querySelector(".sidebar").classList.toggle("show-menu")
+);
+document.querySelector(".menuh").addEventListener("click", () =>
+    document.querySelector(".menuh").classList.toggle("show-menu")
+);
